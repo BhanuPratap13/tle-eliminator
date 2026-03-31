99d7a7902182fdd19e2af16583ae9676076ff5b1
BhanuPratap13 <bhanu13pratap2005@gmail.com>
2026-03-31 11:59:52 +0530

    Day 2 - Line Trip

---
+++ CP-31/800/02_Line_Trip.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int arr[] = new int [n+2];
+            arr[0]=0;
+            for(int i=1;i<=n;i++){
+                arr[i]=sc.nextInt();
+            }
+            arr[n+1]=x;
+            int max = Integer.MIN_VALUE;
+            for(int i=1;i<arr.length;i++){
+                if(i==arr.length-1){
+                    max=Math.max(max,2*(arr[i]-arr[i-1]));
+                }
+                else{
+                    max=Math.max(max,arr[i]-arr[i-1]);
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
+
