1c909c853fc7604fa61da99cc4668b26e1858169
BhanuPratap13 <bhanu13pratap2005@gmail.com>
2026-04-01 11:19:13 +0530

    Day 3 cover in water

---
+++ CP-31/800/cover_in_water.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+ 
+public class cover_in_water {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+ 
+            if (s.contains("...")) {
+                System.out.println(2);
+            } else {
+                int count = 0;
+                for (char c : s.toCharArray()) {
+                    if (c == '.') count++;
+                }
+                System.out.println(count);
+            }
+        }
+    }
+}
